da8198d22e790558d65ea17e18f37999cd2b0830
Uchida-Yuto <c3p31014@bunkyo.ac.jp>
2024-06-06 22:02:36 +0900

    内容編集

--- m_class.php
+++ m_class.php
@@ -22,8 +22,8 @@ class data_list{
 //拾得物リストの管理用クラス
 class find_item_list extends data_list{
   function __construct(){
-    $sql  = "SELECT 拾得物分類.大分類, 拾得物分類.物品名, 拾得物分類.貴重品, ";
-    $sql .= "拾得物.ID AS 拾得物ID, 拾得物.拾得場所, 拾得物.色, 拾得物.特徴, 拾得物.画像, ";
+    $sql  = "SELECT 拾得物分類.分類, 拾得物分類.物品名, 拾得物分類.拾得日, ";
+    $sql .= "拾得物.ID AS 拾得物ID, 拾得物.拾得物分類ID, 拾得物.拾得場所, 拾得物.色, 拾得物.特長, 拾得物.画像, ";
     $sql .= "拾得物管理状況.ID AS 拾得物管理状況ID, 拾得物管理状況.変更日時, 拾得物管理状況.変更内容 ";
     $sql .= "FROM 拾得物管理状況 INNER JOIN ";
     $sql .= "(SELECT 拾得物ID,MAX(変更日時) AS 日時 FROM 拾得物管理状況 GROUP BY 拾得物ID) AS 最終更新 ";
@@ -39,7 +39,7 @@ class find_item_list extends data_list{
   }
 }
 
-//拾得物の詳細情報保持用クラス
+//拾得物の詳細情報保持用クラス(未編集)
 class find_item_detail extends data_list{
   var $変更履歴=array();
   function __construct($fid){
@@ -67,7 +67,7 @@ class find_item_detail extends data_list{
   }
 }
 
-
+//ユーザ情報保持用クラス（編集済み）
 class user_list extends data_list{
   function __construct(){
     $sql  = "SELECT ユーザ.ID AS ユーザID, ユーザ.氏名, ユーザ.電話番号, ユーザ.メールアドレス, ";
@@ -80,9 +80,10 @@ class user_list extends data_list{
   }
 }
 
+//分類情報保持用クラス（編集済み）
 class category_list extends data_list{
   function __construct(){
-    $sql  = "SELECT 拾得物分類.ID AS 拾得物分類ID, 大分類, 物品名, 五十音, 貴重品 ";
+    $sql  = "SELECT 拾得物分類.ID AS 拾得物分類ID, 分類, 物品名, 拾得日, 拾得時間 ";
     $sql .= "FROM 拾得物分類 ";
     $sql .= "ORDER BY 拾得物分類ID;";
     $this->setSQL($sql);
@@ -91,6 +92,29 @@ class category_list extends data_list{
   }
 }
 
+//遺失物捜索依頼情報保持用クラス（編集済み）
+class request_list extends data_list{
+  function __construct(){
+    $sql  = "SELECT 遺失物捜索依頼.ID AS 遺失物捜索依頼ID, ユーザID, 分類, 遺失場所, 色, 特長 ";
+    $sql .= "FROM 遺失物捜索依頼 ";
+    $sql .= "ORDER BY 遺失物捜索依頼ID;";
+    $this->setSQL($sql);
+    $arr = $this->getList();
+    foreach($arr as $row) $this->dlist[] = new item($row);
+  }
+}
+
+//遺失物分類保持用クラス
+class lostitem_category_list extends data_list{
+  function __construct(){
+    $sql  = "SELECT 遺失物分類.ID AS 遺失物分類ID, 分類, 物品名, 遺失日 ";
+    $sql .= "FROM 遺失物分類 ";
+    $sql .= "ORDER BY 遺失物分類ID;";
+    $this->setSQL($sql);
+    $arr = $this->getList();
+    foreach($arr as $row) $this->dlist[] = new item($row);
+  }
+}
 class item{
   function __construct($arg){
     foreach($arg as $i => $v){
